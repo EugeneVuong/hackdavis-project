aadb81d2a1ac23f1211d63f262738acb85426869
Eugene Vuong <vuongeugene@gmail.com>
2024-04-28 12:53:56 -0700

    Model is Working in Flask

--- visionEndpoint/main.py
+++ visionEndpoint/main.py
@@ -1,24 +1,37 @@
-from fastapi import FastAPI, File
-from segmentation import get_yolov5, get_image_from_bytes
-from starlette.responses import Response
-import io
-from PIL import Image
+####################################### IMPORT #################################
 import json
+import pandas as pd
+from PIL import Image
+
+from fastapi import FastAPI, File, status
+from fastapi.responses import RedirectResponse
+from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import HTTPException
 
 
-model = get_yolov5()
+from app import get_image_from_bytes
+from app import detect_sample_model
 
+
+
+###################### FastAPI Setup #############################
+
+# title
 app = FastAPI(
-    title="Custom YOLOV5 Machine Learning API",
+    title="Object Detection FastAPI Template",
     description="""Obtain object value out of image
                     and return image and json result""",
-    version="0.0.1",
+    version="2023.1.31",
 )
 
+# This function is needed if you want to allow client requests 
+# from specific domains (specified in the origins argument) 
+# to access resources from the FastAPI server, 
+# and the client and server are hosted on different domains.
 origins = [
     "http://localhost",
-    "http://localhost:8000",
+    "http://localhost:8008",
     "*"
 ]
 
@@ -30,45 +43,85 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# redirect
+@app.get("/", include_in_schema=False)
+async def redirect():
+    return RedirectResponse("/docs")
 
-@app.get('/notify/v1/health')
-def get_health():
+
+@app.get('/healthcheck', status_code=status.HTTP_200_OK)
+def perform_healthcheck():
+    '''
+    It basically sends a GET request to the route & hopes to get a "200"
+    response code. Failing to return a 200 response code just enables
+    the GitHub Actions to rollback to the last version the project was
+    found in a "working condition". It acts as a last line of defense in
+    case something goes south.
+    Additionally, it also returns a JSON response in the form of:
+    {
+        'healtcheck': 'Everything OK!'
+    }
+    '''
+    return {'healthcheck': 'Everything OK!'}
+
+
+######################### Support Func #################################
+
+def crop_image_by_predict(image: Image, predict: pd.DataFrame(), crop_class_name: str,) -> Image:
+    """Crop an image based on the detection of a certain object in the image.
+    
+    Args:
+        image: Image to be cropped.
+        predict (pd.DataFrame): Dataframe containing the prediction results of object detection model.
+        crop_class_name (str, optional): The name of the object class to crop the image by. if not provided, function returns the first object found in the image.
+    
+    Returns:
+        Image: Cropped image or None
     """
-    Usage on K8S
-    readinessProbe:
-        httpGet:
-            path: /notify/v1/health
-            port: 80
-    livenessProbe:
-        httpGet:
-            path: /notify/v1/health
-            port: 80
-    :return:
-        dict(msg='OK')
+    crop_predicts = predict[(predict['name'] == crop_class_name)]
+
+    if crop_predicts.empty:
+        raise HTTPException(status_code=400, detail=f"{crop_class_name} not found in photo")
+
+    # if there are several detections, choose the one with more confidence
+    if len(crop_predicts) > 1:
+        crop_predicts = crop_predicts.sort_values(by=['confidence'], ascending=False)
+
+    crop_bbox = crop_predicts[['xmin', 'ymin', 'xmax','ymax']].iloc[0].values
+    # crop
+    img_crop = image.crop(crop_bbox)
+    return(img_crop)
+
+
+######################### MAIN Func #################################
+
+
+@app.post("/img_object_detection_to_json")
+def img_object_detection_to_json(file: bytes = File(...)):
     """
-    return dict(msg='OK')
+    Object Detection from an image.
 
+    Args:
+        file (bytes): The image file in bytes format.
+    Returns:
+        dict: JSON format containing the Objects Detections.
+    """
+    # Step 1: Initialize the result dictionary with None values
+    result={'detect_objects': None}
 
-@app.post("/object-to-json")
-async def detect_wound_return_json_result(file: bytes = File(...)):
+    # Step 2: Convert the image file to an image object
     input_image = get_image_from_bytes(file)
 
-    results = model(input_image)
+    # Step 3: Predict from model
+    predict = detect_sample_model(input_image)
 
-    detect_res = results.pandas().xyxy[0].to_json(orient="records")  # JSON img1 predictions
+    # Step 4: Select detect obj return info
+    # here you can choose what data to send to the result
+    detect_res = predict[['name', 'confidence']]
+    objects = detect_res['name'].values
 
-    detect_res = json.loads(detect_res)
+    result['detect_objects_names'] = ', '.join(objects)
+    result['detect_objects'] = json.loads(detect_res.to_json(orient='records'))
 
-    return {"result": detect_res}
-
-
-@app.post("/object-to-img")
-async def detect_wound_return_base64_img(file: bytes = File(...)):
-    input_image = get_image_from_bytes(file)
-    results = model(input_image)
-    results.render()  # updates results.imgs with boxes and labels
-    for img in results.ims:
-        bytes_io = io.BytesIO()
-        img_base64 = Image.fromarray(img)
-        img_base64.save(bytes_io, format="jpeg")
-    return Response(content=bytes_io.getvalue(), media_type="image/jpeg")
+    # Step 5: Logs and return
+    return result
