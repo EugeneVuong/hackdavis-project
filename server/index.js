8e50b538564d7f5fa283d634fb98f2653013b0f9
notnuthin <116135015+notnuthin@users.noreply.github.com>
2024-04-27 21:04:17 -0700

    Added Supabase

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 import express from 'express'
+import supabase from './supabaseClient.js'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.send('Hello World!')
+  const {data, error} = await supabase
+  .from('smoothies')
+  .select()
+
+  console.log(data)
+  const title = 'Spicy Shah'
+  const method = 'Mango with Prickly Pear'
+  const rating = 11
+  const{data1,error1} = await supabase
+  .from('smoothies')
+  .insert([{title, method, rating }])
+
+  console.log(data1)
 })
 
 app.get('/he', (req, res) => {
